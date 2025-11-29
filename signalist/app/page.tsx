d603ba046faf7c56fcce1f7b6d7ca6b9b3fbabbb
sairishigangarapu <sairishigangarapu@gmail.com>
2025-11-29 20:53:20 +0530

    First Commit

---
+++ signalist/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Button>
+        Click Me
+      </Button>
+    </div>
+  )
+}
+
+export default page
