da42b809acf5bdbb61a398f70767f0f4e6d699d1
sairishigangarapu <sairishigangarapu@gmail.com>
2025-12-05 20:46:01 +0530

    Header implemented

---
+++ signalist/components/NavItems.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { NAV_ITEMS } from '@/lib/constants'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+function NavItems() {
+  const pathname = usePathname()
+
+  return (
+    <ul className='flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium'>
+      {NAV_ITEMS.map((item) => (
+        <li key={item.href}>
+            <Link href={item.href} className={`hover:text-yellow-500 transition-colors ${
+                pathname === item.href ? 'text-gray-100':''
+            }`}>
+            {item.title}
+            </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default NavItems
